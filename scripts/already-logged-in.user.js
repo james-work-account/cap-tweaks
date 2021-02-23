7fcb30109aec6f22855f43089413dcec489eb1a4
James Whiteley <31282758+james-work-account@users.noreply.github.com>
2021-02-23 14:02:27 +0000

    2021 fix
    The selector to remove the obtrusive popup was changed at some point - this commit should address that.

--- scripts/already-logged-in.user.js
+++ scripts/already-logged-in.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Fix Already Logged In
 // @namespace    james-work-account
-// @version      0.1
+// @version      0.2
 // @description  Sometimes when you refresh the page or use the browser's "Back" button, it locks you out claiming it "is open in another browser window / tab. Please close all browsers and try again." - this script fixes this.
 // @author       James Whiteley
 // @match        https://myconnect.capgemini.com/sap/bc/ui5_ui5/sap/zmypath/index.html*
@@ -12,21 +12,29 @@
 
 let popupBlock;
 const removeLoggedInPopups = () => {
-  popupBlock.classList.remove("sapUiBLy");
-  document.getElementById("__mbox0").innerHTML = "";
-  document.querySelector("html").classList.remove("sapUiBLyBack");
+    popupBlock.classList.remove("sapUiBLy");
+    const boxes = Array.from(document.querySelectorAll("#__mbox0"))
+    for(const box of boxes) {
+        box.style.border = "";
+        box.style.display = "none";
+        box.style.height = 0;
+        box.style.width = 0;
+        box.innerHTML = "";
+    }
+    document.getElementById("sap-ui-static").innerHTML = "";
+    document.querySelector("html").classList.remove("sapUiBLyBack");
 }
 
 (function() {
-  'use strict';
-  const repeating = setInterval(function() {
-    popupBlock = document.getElementById("sap-ui-blocklayer-popup");
-    if(typeof popupBlock !== "undefined") {
-      removeLoggedInPopups();
-      clearInterval(repeating);
-    }
-  }, 100);
+    'use strict';
+    const repeating = setInterval(function() {
+        popupBlock = document.getElementById("sap-ui-blocklayer-popup");
+        if(typeof popupBlock !== "undefined" && popupBlock !== null) {
+            removeLoggedInPopups();
+            clearInterval(repeating);
+        }
+    }, 100);
 
-  // stop after 10 seconds if the element never exists - it is futile (the issue isn't there in the first place or something else went wrong with the script)
-  setTimeout(() => { clearInterval(repeating); }, 10000);
+    // stop after 10 seconds if the element never exists - it is futile (the issue isn't there in the first place or something else went wrong with the script)
+    setTimeout(() => { clearInterval(repeating); }, 10000);
 })();
